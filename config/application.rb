968880d3fbcbf2c547224739d06bd96d7eaeffa1
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2018-11-10 19:57:56 +0900

    Review settings👀

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,6 @@ module RailsStudyBot
   class Application < Rails::Application
     config.load_defaults 5.2
     config.time_zone = 'Tokyo'
-    config.active_record.default_timezone = :local
     config.generators do |g|
       g.assets      false
       g.helper      false
