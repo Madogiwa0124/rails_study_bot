47c9afcff4000ce90528536e5e8e1e5d9474e1e2
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2018-11-10 18:37:30 +0900

    rails new and initialize setting 🎉

---
+++ config/application.rb
@@ -0,0 +1,33 @@
+require_relative 'boot'
+
+require "rails"
+require "active_model/railtie"
+require "active_job/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
+
+Bundler.require(*Rails.groups)
+
+module RailsStudyBot
+  class Application < Rails::Application
+    config.load_defaults 5.2
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.generators do |g|
+      g.assets      false
+      g.helper      false
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: false,
+        request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
+  end
+end
