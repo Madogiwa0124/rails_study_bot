eb6f18f63639a15e5456a7e340a0eaf57d0535bd
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2018-11-11 17:02:59 +0900

    add TwitterClient🐦

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'uglifier', '>= 1.3.0'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
+gem 'twitter'
+gem 'dotenv'
 
 group :development, :test do
   gem 'pry-rails'
---
+++ app/models/twitter_client.rb
@@ -0,0 +1,32 @@
+class TwitterClient
+  require 'dotenv/load'
+  
+  def initialize
+    # クライアントの生成
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['CONSUMER_KEY']
+      config.consumer_secret     = ENV['CONSUMER_SECRET']
+      config.access_token        = ENV['ACCESS_TOKEN']
+      config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+    end
+  end
+
+  # Tweetの投稿処理呼び出し
+  def tweet
+    @client.update(text)
+  rescue => e
+    p e # エラー時はログを出力
+  end
+
+  private
+
+  # ツイート本文の生成
+  def text
+    sample_rails_method_message
+  end
+
+  def sample_rails_method_message
+    # TODO: ActiveSupport以外もつぶやくようにする
+    MessageBuilder::ActiveSupport.build
+  end
+end
