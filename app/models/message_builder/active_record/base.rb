fab16c42e9636eb07ca5ae4b73d6e93f59a882b7
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2018-11-11 16:31:55 +0900

    add spec MessageBuilder::ActiveSupport✨

---
+++ app/models/message_builder/active_record/base.rb
@@ -0,0 +1,62 @@
+class MessageBuilder::ActiveRecord::Base
+  require "active_record"
+  
+  RAILS_VERSION = '5-2-1'
+
+  def self.build
+    self.new.message
+  end
+
+  def initialize
+    init_method_location
+    make_method_and_location
+  end
+
+  def message
+    <<~"EOS"
+      rails(ver #{RAILS_VERSION})のメソッドをコードを読んで勉強しよう🙌
+      class: ActiveRecord::Base
+      method: #{@method}
+      url: #{github_url}
+    EOS
+  end
+
+  private
+
+  def sample_method
+    @method = ActiveRecord::Base.public_methods(false).sample
+  end
+
+  def source_location
+    @location = ActiveRecord::Base.method(@method)&.source_location
+  end
+
+  def make_method_and_location
+    while @location.nil? do
+      sample_method
+      source_location
+    end
+  end
+
+  def source_path
+    { path: @location[0].match(/active_record.*/)[0], line_no: @location[1] }
+  rescue StandardError
+    # error発生時は、メソッドとロケーションを再取得する。
+    init_method_location
+    make_method_and_location
+    source_path
+  end
+
+  def init_method_location
+    @method = ""
+    @location = nil
+  end
+
+  def github_url
+    "#{github_base_url + source_path[:path]}#L#{source_path[:line_no]}"
+  end
+
+  def github_base_url
+    "https://github.com/rails/rails/blob/#{RAILS_VERSION}/activerecord/lib/"
+  end
+end
