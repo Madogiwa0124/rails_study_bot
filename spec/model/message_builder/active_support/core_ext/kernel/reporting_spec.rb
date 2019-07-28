bf63d99b912b9670721229a2d1df666ede964bd5
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2019-07-28 18:58:42 +0900

    add kernel core extension

---
+++ spec/model/message_builder/active_support/core_ext/kernel/reporting_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe MessageBuilder::ActiveSupport::CoreExt::Kernel::Reporting, type: :model do
+  describe '.build' do
+    it 'classが表示されること' do
+      expect(described_class.build.include?('class: Kernel')).to eq true
+    end
+
+    it 'methodが表示されること' do
+      expect(described_class.build.include?('method:')).to eq true
+    end
+
+    it 'urlが表示されること' do
+      expect(described_class.build.include?('url:')).to eq true
+    end
+  end
+end
