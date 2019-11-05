3b11477161aa73100e3527f31d4a1f9ce47764d7
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2019-11-05 22:16:04 +0900

    :recycle: use MessageBuilder::Base.subclass.

--- app/models/twitter_client.rb
+++ app/models/twitter_client.rb
@@ -25,39 +25,7 @@ class TwitterClient
     sample_rails_method_message
   end
 
-  # rubocop:disable Metrics/MethodLength
   def sample_rails_method_message
-    [
-      MessageBuilder::ActiveSupport,
-      MessageBuilder::ActiveSupport::CoreExt::DateAndTime::Calculations,
-      MessageBuilder::ActiveSupport::CoreExt::Array,
-      MessageBuilder::ActiveSupport::CoreExt::Class,
-      MessageBuilder::ActiveSupport::CoreExt::DateTime,
-      MessageBuilder::ActiveSupport::CoreExt::Date,
-      MessageBuilder::ActiveSupport::CoreExt::Enumerable,
-      MessageBuilder::ActiveSupport::CoreExt::File,
-      MessageBuilder::ActiveSupport::CoreExt::Hash,
-      MessageBuilder::ActiveSupport::CoreExt::Integer,
-      MessageBuilder::ActiveSupport::CoreExt::Kernel,
-      MessageBuilder::ActiveSupport::CoreExt::Module,
-      MessageBuilder::ActiveSupport::CoreExt::Numeric,
-      MessageBuilder::ActiveSupport::CoreExt::Object,
-      MessageBuilder::ActiveSupport::CoreExt::Range,
-      MessageBuilder::ActiveSupport::CoreExt::String,
-      MessageBuilder::ActiveSupport::CoreExt::Time,
-      MessageBuilder::ActiveSupport::Dependencies,
-      MessageBuilder::ActiveRecord::Base,
-      MessageBuilder::ActiveRecord::Relation::QueryMethods,
-      MessageBuilder::ActiveRecord::Relation::Calculations,
-      MessageBuilder::ActiveRecord::Relation::FinderMethods,
-      MessageBuilder::ActiveRecord::Relation::Batches,
-      MessageBuilder::Actionpack::ActionController::Base,
-      MessageBuilder::ActionView::Base,
-      MessageBuilder::ActionView::Helpers::FormTagHelper,
-      MessageBuilder::ActionView::Helpers::FormOptionsHelper,
-      MessageBuilder::ActionView::Helpers::AssetTagHelper,
-      MessageBuilder::ActionView::Helpers::AssetUrlHelper
-    ].sample.build
-    # rubocop:enable Metrics/MethodLength
+    MessageBuilder::Base.subclasses.sample.build
   end
 end
