5be9ec6f6cb49069916a64eb1f8ff555cc10ba49
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2020-03-22 17:12:07 +0900

    :sparkles: add Actionpack::ActionDispatch::Journey::Router

---
+++ app/models/message_builder/actionpack/action_dispatch/journey/router.rb
@@ -0,0 +1,6 @@
+class MessageBuilder::Actionpack::ActionDispatch::Journey::Router < MessageBuilder::Base
+  RAILS_CLASS = ActionDispatch::Journey::Router
+  RAILS_TOP_CLASS_NAME = 'actionpack'.freeze
+  RAILS_CLASS_FILE_REGXP = %r{action_dispatch/journey/router.*}.freeze
+  RAILS_CLASS_REGXP = /router/.freeze
+end
